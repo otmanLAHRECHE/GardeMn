d02e15e28bfec7b4b9f8952ca849b5f986d19e51
otman lahreche <otman.lahreche@gmail.com>
2023-08-31 14:42:21 +0100

    update

--- GardeM/app/views.py
+++ GardeM/app/views.py
@@ -50,7 +50,63 @@ def createNewWorker(request):
         else:
             return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
+
+
+@api_view(['POST'])
+def updateWorker(request, id):
+    if request.method == 'POST' and request.user.is_authenticated:
+
+        name = request.data.pop('name')
+        prename = request.data.pop('prename')
+        service = request.data.pop('service')
+        grade = request.data.pop('grade')
+        ccp = request.data.pop('ccp')
+        
+        worker_to_update = Workers.objects.get(id = id)
+
+        if not worker_to_update.name == name:
+            worker_to_update.name = name
+        if not worker_to_update.prename == prename:
+            worker_to_update.prename = prename
+        if not worker_to_update.service == service:
+            worker_to_update.service = service
+        if not worker_to_update.grade == grade:
+            worker_to_update.grade = grade
+        if not worker_to_update.ccp == ccp:
+            worker_to_update.ccp = ccp
+
+        
+        worker_to_update.save()
+
+        
+        return Response(status=status.HTTP_200_OK, data = {"status":"worker updated"})
+    else:
+        return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
     
+
+
+
+@api_view(['GET'])
+def getSelectedWorker(request, id):
+    if request.method == 'GET' and request.user.is_authenticated:
+        queryset = Workers.objects.get(id = id)
+
+        source_serial = WorkerSerializer(queryset)
+
+        return Response(status=status.HTTP_200_OK,data=source_serial.data)
+                
+    
+    else :
+        return Response(status=status.HTTP_401_UNAUTHORIZED)
+    
+
+@api_view(['DELETE'])
+def deleteWorker(request, id):
+    if request.method == 'DELETE' and request.user.is_authenticated:
+        Workers.objects.filter(id=id).delete()
+        return Response(status=status.HTTP_200_OK, data = {"status":"Worker deleted"})
+
+  
 
 
 
