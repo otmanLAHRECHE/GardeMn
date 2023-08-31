ecc15ef41d54c0e058d82e547447aaf4ee68893c
otman lahreche <otman.lahreche@gmail.com>
2023-08-31 10:36:03 +0100

    update

---
+++ GardeM/accounts/urls.py
@@ -0,0 +1,9 @@
+
+from django.urls import path, include
+
+
+urlpatterns = [
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
+    path('auth/', include('djoser.urls.jwt')),
+]
